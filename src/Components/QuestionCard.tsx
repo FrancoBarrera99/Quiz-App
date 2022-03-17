26c5d9880d7d1e973f4d72d5c7288fb24ccc92c4
FrancoBarrera99 <56893886+FrancoBarrera99@users.noreply.github.com>
2022-03-17 13:35:45 -0300

    Question card component, fetch data from API, random sort utils function

---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNumber: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ question,
+    answers,
+    callback,
+    userAnswer,
+    questionNumber,
+    totalQuestions
+}) => (
+    <div>
+        <p className="questionNumber">
+            Question: {questionNumber} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }}></p>
+        <div>
+            {answers.map(answer => (
+                <div>
+                    <button disabled={userAnswer} onClick={callback}>
+                        <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+                    </button>
+                </div>
+            ))}
+        </div>
+    </div>
+);
+
+export default QuestionCard;
